29c9e6af3de5ca4fcd77a01b65b059930230313c
a1da4 <ai.the.taichi@gmail.com>
2020-09-26 23:12:46 +0900

    add map word2id, id2word test

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <map>
 #include <Eigen/Dense>
 using namespace std;
 using Eigen::MatrixXd;
@@ -16,17 +17,38 @@ int main(){
     m(1,1) = m(1,0) + m(0,1);
     cout << m << endl;
 
+
     // test readfile
     cout << "test readfile" << endl;
     ifstream ifs("./../test/input.txt");
+
+    // test id2word, word2id
+    cout << "test id2word, word2id" << endl;
+    map<string, int>word2id;
+    map<int, string>id2word;
+    int id = 0;
     string sentence;
     string word;
     while (getline(ifs, sentence)) {
         istringstream iss(sentence);
         while (iss >> word) {
-            cout << word << endl;
+            //cout << word << endl;
+            if (word2id.count(word) == 0) {
+                word2id.insert(make_pair(word, id));
+                id2word.insert(make_pair(id, word));
+                id++;
+            }
         }
     }
-}
+    cout << "print word2id" << endl;
+    for (map<string, int>::iterator i=word2id.begin(); i!=word2id.end(); ++i) {
+        cout << i->first << " => " << i->second << endl;
+    }
+    cout << "print id2word" << endl;
+    for (map<int, string>::iterator i=id2word.begin(); i!=id2word.end(); ++i) {
+        cout << i->first << " => " << i->second << endl;
+    }
 
+    return 0;
+}
 
