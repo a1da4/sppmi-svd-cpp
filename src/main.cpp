3b7294bafa444f422e4cd91798b482b4afb46174
a1da4 <ai.the.taichi@gmail.com>
2020-09-26 21:16:07 +0900

    add input test

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <Eigen/Dense>
 using namespace std;
 using Eigen::MatrixXd;
 
 int main(){
-  MatrixXd m(2,2);
-  m(0,0) = 3;
-  m(1,0) = 2.5;
-  m(0,1) = -1;
-  m(1,1) = m(1,0) + m(0,1);
-  cout << m << endl;
+    // test Eigen
+    cout << "test Eigen" << endl;
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    cout << m << endl;
+
+    // test readfile
+    cout << "test readfile" << endl;
+    ifstream ifs("./../test/input.txt");
+    string sentence;
+    string word;
+    while (getline(ifs, sentence)) {
+        istringstream iss(sentence);
+        while (iss >> word) {
+            cout << word << endl;
+        }
+    }
 }
 
 
